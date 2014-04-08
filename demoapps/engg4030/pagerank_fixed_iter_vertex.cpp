ca4948d121a341c0cfcee39de586d9141fd4b965
HU Pili <hpl1989@gmail.com>
2014-04-08 04:21:50 +0000

    fixed iter by vertex scheduling

--- demoapps/engg4030/pagerank_fixed_iter_vertex.cpp
+++ demoapps/engg4030/pagerank_fixed_iter_vertex.cpp
@@ -35,15 +35,28 @@
 
 float RESET_PROB = 0.15;
 float TOLERANCE = 1.0E-5;
+int SCHEDULE_COUNT = 10;
 
 int g_num_vertices = -1;
 int g_num_edges = -1;
 
-typedef float vertex_data_type;
+typedef struct _vertex_data_type{
+	float value; // Current PR value
+	int count; // # of times the vertex is scheduled
+	void save(graphlab::oarchive& oarc) const {
+		oarc << count << value;
+	}
+	void load(graphlab::iarchive& iarc) {
+		iarc >> count >> value;
+	}
+} vertex_data_type;
 typedef graphlab::empty edge_data_type;
 typedef graphlab::distributed_graph<vertex_data_type, edge_data_type> graph_type;
 
-void init_vertex(graph_type::vertex_type& vertex) { vertex.data() = 1; }
+void init_vertex(graph_type::vertex_type& vertex) {
+	vertex.data().value = 1;
+	vertex.data().count = 0;
+}
 
 class pagerank :
 	public graphlab::ivertex_program<graph_type, float>,
@@ -58,17 +71,18 @@ class pagerank :
 	float gather(icontext_type& context, const vertex_type& vertex,
 			edge_type& edge) const {
 		return ((1.0 - RESET_PROB) / edge.source().num_out_edges()) *
-			edge.source().data();
+			edge.source().data().value;
 	}
 	void apply(icontext_type& context, vertex_type& vertex,
 			const gather_type& total) {
 		const double newval = total + RESET_PROB;
-		last_change = std::fabs(newval - vertex.data());
-		vertex.data() = newval;
+		last_change = std::fabs(newval - vertex.data().value);
+		vertex.data().value = newval;
+		vertex.data().count += 1;
 	}
 	edge_dir_type scatter_edges(icontext_type& context,
 			const vertex_type& vertex) const {
-		if (last_change > TOLERANCE) return graphlab::OUT_EDGES;
+		if (vertex.data().count < SCHEDULE_COUNT) return graphlab::OUT_EDGES;
 		else return graphlab::NO_EDGES;
 	}
 	void scatter(icontext_type& context, const vertex_type& vertex,
@@ -81,7 +95,7 @@ class pagerank :
 struct pagerank_writer {
 	std::string save_vertex(graph_type::vertex_type v) {
 		std::stringstream strm;
-		strm << "vertex:" << v.id() << "\t" << v.data() / g_num_vertices << "\n";
+		strm << "vertex:" << v.id() << "\t" << v.data().value / g_num_vertices << "\n";
 		return strm.str();
 	}
 	std::string save_edge(graph_type::edge_type e) { return ""; }
@@ -97,7 +111,7 @@ int main(int argc, char** argv) {
 	std::string graph_dir = "sample_tsv";
 	std::string format = "tsv";
 	std::string exec_type = "synchronous";
-	std::string saveprefix = "sample_output/pr_base";
+	std::string saveprefix = "sample_output/pr_fixed_iter_vertex";
 
 	graph_type graph(dc, clopts);
 	dc.cout() << "Loading graph in format: "<< format << std::endl;
