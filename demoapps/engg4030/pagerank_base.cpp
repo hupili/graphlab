082f33766820d4908d241222cca575b05c9bbcaf
HU Pili <hpl1989@gmail.com>
2014-04-08 03:24:00 +0000

    VIM auto indent

--- demoapps/engg4030/pagerank_base.cpp
+++ demoapps/engg4030/pagerank_base.cpp
@@ -43,105 +43,105 @@ typedef graphlab::distributed_graph<vertex_data_type, edge_data_type> graph_type
 void init_vertex(graph_type::vertex_type& vertex) { vertex.data() = 1; }
 
 class pagerank :
-  public graphlab::ivertex_program<graph_type, float>,
-  public graphlab::IS_POD_TYPE {
-  float last_change;
-public:
-  float gather(icontext_type& context, const vertex_type& vertex,
-               edge_type& edge) const {
-    return ((1.0 - RESET_PROB) / edge.source().num_out_edges()) *
-      edge.source().data();
-  }
+	public graphlab::ivertex_program<graph_type, float>,
+	public graphlab::IS_POD_TYPE {
+		float last_change;
+		public:
+		float gather(icontext_type& context, const vertex_type& vertex,
+				edge_type& edge) const {
+			return ((1.0 - RESET_PROB) / edge.source().num_out_edges()) *
+				edge.source().data();
+		}
 
-  void apply(icontext_type& context, vertex_type& vertex,
-             const gather_type& total) {
-    const double newval = total + RESET_PROB;
-    last_change = std::fabs(newval - vertex.data());
-    vertex.data() = newval;
-  }
+		void apply(icontext_type& context, vertex_type& vertex,
+				const gather_type& total) {
+			const double newval = total + RESET_PROB;
+			last_change = std::fabs(newval - vertex.data());
+			vertex.data() = newval;
+		}
 
-  edge_dir_type scatter_edges(icontext_type& context,
-                              const vertex_type& vertex) const {
-    if (last_change > TOLERANCE) return graphlab::OUT_EDGES;
-    else return graphlab::NO_EDGES;
-  }
+		edge_dir_type scatter_edges(icontext_type& context,
+				const vertex_type& vertex) const {
+			if (last_change > TOLERANCE) return graphlab::OUT_EDGES;
+			else return graphlab::NO_EDGES;
+		}
 
-  void scatter(icontext_type& context, const vertex_type& vertex,
-               edge_type& edge) const {
-    context.signal(edge.target());
-  }
-}; // end of factorized_pagerank update functor
+		void scatter(icontext_type& context, const vertex_type& vertex,
+				edge_type& edge) const {
+			context.signal(edge.target());
+		}
+	}; // end of factorized_pagerank update functor
 
 
 struct pagerank_writer {
-  std::string save_vertex(graph_type::vertex_type v) {
-    std::stringstream strm;
-    strm << "vertex:" << v.id() << "\t" << v.data() << "\n";
-    return strm.str();
-  }
-  std::string save_edge(graph_type::edge_type e) { return ""; }
+	std::string save_vertex(graph_type::vertex_type v) {
+		std::stringstream strm;
+		strm << "vertex:" << v.id() << "\t" << v.data() << "\n";
+		return strm.str();
+	}
+	std::string save_edge(graph_type::edge_type e) { return ""; }
 }; // end of pagerank writer
 
 int main(int argc, char** argv) {
-  graphlab::mpi_tools::init(argc, argv);
-  graphlab::distributed_control dc;
-  global_logger().set_log_level(LOG_INFO);
+	graphlab::mpi_tools::init(argc, argv);
+	graphlab::distributed_control dc;
+	global_logger().set_log_level(LOG_INFO);
 
-  // Parse command line options -----------------------------------------------
-  graphlab::command_line_options clopts("PageRank algorithm.");
-  std::string graph_dir;
-  std::string format = "adj";
-  std::string exec_type = "synchronous";
-  clopts.attach_option("graph", graph_dir,
-                       "The graph file. Required ");
-  clopts.add_positional("graph");
-  clopts.attach_option("format", format,
-                       "The graph file format");
-  clopts.attach_option("engine", exec_type, 
-                       "The engine type synchronous or asynchronous");
-  clopts.attach_option("tol", TOLERANCE,
-                       "The permissible change at convergence.");
-  std::string saveprefix;
-  clopts.attach_option("saveprefix", saveprefix,
-                       "If set, will save the resultant pagerank to a "
-                       "sequence of files with prefix saveprefix");
+	// Parse command line options -----------------------------------------------
+	graphlab::command_line_options clopts("PageRank algorithm.");
+	std::string graph_dir;
+	std::string format = "adj";
+	std::string exec_type = "synchronous";
+	clopts.attach_option("graph", graph_dir,
+			"The graph file. Required ");
+	clopts.add_positional("graph");
+	clopts.attach_option("format", format,
+			"The graph file format");
+	clopts.attach_option("engine", exec_type, 
+			"The engine type synchronous or asynchronous");
+	clopts.attach_option("tol", TOLERANCE,
+			"The permissible change at convergence.");
+	std::string saveprefix;
+	clopts.attach_option("saveprefix", saveprefix,
+			"If set, will save the resultant pagerank to a "
+			"sequence of files with prefix saveprefix");
 
-  if(!clopts.parse(argc, argv)) {
-    dc.cout() << "Error in parsing command line arguments." << std::endl;
-    return EXIT_FAILURE;
-  }
+	if(!clopts.parse(argc, argv)) {
+		dc.cout() << "Error in parsing command line arguments." << std::endl;
+		return EXIT_FAILURE;
+	}
 
-  if (graph_dir == "") {
-    dc.cout() << "Graph not specified. Cannot continue";
-    return EXIT_FAILURE;
-  }
+	if (graph_dir == "") {
+		dc.cout() << "Graph not specified. Cannot continue";
+		return EXIT_FAILURE;
+	}
 
-  graph_type graph(dc, clopts);
-  dc.cout() << "Loading graph in format: "<< format << std::endl;
-  graph.load_format(graph_dir, format);
+	graph_type graph(dc, clopts);
+	dc.cout() << "Loading graph in format: "<< format << std::endl;
+	graph.load_format(graph_dir, format);
 
-  graph.finalize();
-  dc.cout() << "#vertices: " << graph.num_vertices()
-            << " #edges:" << graph.num_edges() << std::endl;
+	graph.finalize();
+	dc.cout() << "#vertices: " << graph.num_vertices()
+		<< " #edges:" << graph.num_edges() << std::endl;
 
-  graph.transform_vertices(init_vertex);
+	graph.transform_vertices(init_vertex);
 
-  graphlab::omni_engine<pagerank> engine(dc, graph, exec_type, clopts);
-  engine.signal_all();
-  engine.start();
-  const float runtime = engine.elapsed_seconds();
-  dc.cout() << "Finished Running engine in " << runtime
-            << " seconds." << std::endl;
+	graphlab::omni_engine<pagerank> engine(dc, graph, exec_type, clopts);
+	engine.signal_all();
+	engine.start();
+	const float runtime = engine.elapsed_seconds();
+	dc.cout() << "Finished Running engine in " << runtime
+		<< " seconds." << std::endl;
 
-  if (saveprefix != "") {
-    graph.save(saveprefix, pagerank_writer(),
-               false,    // do not gzip
-               true,     // save vertices
-               false);   // do not save edges
-  }
+	if (saveprefix != "") {
+		graph.save(saveprefix, pagerank_writer(),
+				false,    // do not gzip
+				true,     // save vertices
+				false);   // do not save edges
+	}
 
-  graphlab::mpi_tools::finalize();
-  return EXIT_SUCCESS;
+	graphlab::mpi_tools::finalize();
+	return EXIT_SUCCESS;
 } // End of main
 
 
