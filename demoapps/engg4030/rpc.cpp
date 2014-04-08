238d2a7da73e79356f057bc55158574f727e2563
HU Pili <hpl1989@gmail.com>
2014-04-08 05:41:26 +0000

    built-in random generator of graphlab

--- demoapps/engg4030/rpc.cpp
+++ demoapps/engg4030/rpc.cpp
@@ -6,14 +6,16 @@
 
 #include <graphlab.hpp>
 #include <iostream>
-#include <stdlib.h>
-#include <time.h>
 
 graphlab::procid_t g_my_id = -1;
 graphlab::procid_t g_num_procs = -1;
 
 void print_by_core(const std::string& msg){
 	std::cout<< "[proc " << g_my_id << "]: " << msg << std::endl;
+}
+
+float rand_u01(){
+	return graphlab::random::rand01();
 }
 
 int main(int argc, char** argv) {
@@ -28,12 +30,17 @@ int main(int argc, char** argv) {
 	print_by_core("I'm online!");
 	dc.barrier();
 
+	dc.cout() << "Random numbers:" << rand_u01() << "\n" ;
+	dc.cout() << "Random numbers:" << rand_u01() << "\n" ;
+	dc.cout() << "Random numbers:" << rand_u01() << "\n" ;
+	dc.cout() << "Random numbers:" << rand_u01() << "\n" ;
+
 	// Every core will have different seed
 	srand(g_my_id + clock());
 	//std::string msg = "";
 	std::stringstream msg;
 	msg << "Greetings from core: " << g_my_id; 
-	dc.remote_call(rand() % g_num_procs, print_by_core, msg.str());
+	dc.remote_call(int(rand_u01() * g_num_procs), print_by_core, msg.str());
 	dc.barrier();
 
 	graphlab::mpi_tools::finalize();
